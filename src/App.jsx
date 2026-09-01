a768cb48e3f297e22720b5fffd2dd9f867cf3f46
mariamwael1 <mariamwaelabdelaziz12@gmail.com>
2026-09-01 17:03:40 +0300

    Updated3 Day 15 page

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,29 @@ import "./App.css";
 function App() {
   return (
     <div className="container">
-      <h1>{import.meta.env.VITE_APP_NAME}</h1>
 
-      <p>I am learning how React projects are created and managed.</p>
+      <h1>Day 15 - React Project Setup</h1>
 
-      <h2>What I learned:</h2>
-                    
-      <p>Day 15 is complete!</p>
-      
+      <p>
+        I am learning how React projects are created,
+        configured, managed and published.
+      </p>
+
+      <h2>What I Learned</h2>
+
       <ul>
         <li>Vite</li>
         <li>npm</li>
-        <li>package.json</li>
-        <li>Dependencies</li>
-        <li>Environment Variables</li>
-        <li>Git</li>
-        <li>GitHub</li>
+        <li> package.json</li>
+        <li> Dependencies</li>
+        <li> Environment Variables</li>
+        <li> Git</li>
+        <li> GitHub</li>
       </ul>
+
     </div>
   );
 }
 
 export default App;
+
