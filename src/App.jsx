8e544773e6da6200db1ff380b4ea4d59f6a5fe64
mariamwael1 <mariamwaelabdelaziz12@gmail.com>
2026-09-01 13:29:11 +0300

    Initial React project setup

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>{import.meta.env.VITE_APP_NAME}</h1>
+
+      <p>I am learning how React projects are created and managed.</p>
+
+      <h2>What I learned:</h2>
+
+      <ul>
+        <li>Vite</li>
+        <li>npm</li>
+        <li>package.json</li>
+        <li>Dependencies</li>
+        <li>Environment Variables</li>
+        <li>Git</li>
+        <li>GitHub</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
